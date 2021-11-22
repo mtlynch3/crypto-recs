b79341e09620605a332c3691e758a61c449adebb
Melissa <lynch.melissat@gmail.com>
2021-11-22 14:38:47 -0500

    setting up basic styling for App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+const container = {
+  display: 'flex',
+  flexDirection: 'row',
+  height: 400
+}
+
+const offersContainer = {
+  width: '60%',
+}
+
+const offers = {
+  height: '30%',
+  backgroundColor: '#fef7ff',
+  borderRadius: '20px',
+  border: '3px solid pink',
+  margin: '10px',
+  padding: '10px'
+}
+
+const recs = {
+  height: '85%',
+  width: '40%',
+  backgroundColor: '#fef7ff',
+  borderRadius: '20px',
+  border: '3px solid pink',
+  margin: '10px',
+  padding: '10px'  
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Crypto Recs</h1>
+      <div style={container}> 
+        <div style={offersContainer}>
+          <div style={offers}>Best buying offer:</div>
+          <div style={offers}>Best selling offer:</div>
+        </div>
+        <div style={recs}>Recommendations</div>
+      </div>
     </div>
   );
 }
