3d911e9fd6067e433f5d3ea0ab20fc8f03fb2ba0
Melissa <lynch.melissat@gmail.com>
2021-11-29 15:01:31 -0500

    display dummy data with exchange view fn

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -19,6 +19,66 @@ const columnHeaders = {
   justifyContent: 'space-between'
 }
 
+let data = {
+  coinbase: {
+    buyPrice: 5.0,
+    sellPrice: 5.0,
+    buySize: 0.5,
+    sellSize: 0.5,
+  },
+  bitstamp: {
+    buyPrice: 10.0,
+    sellPrice: 10.0,
+    buySize: 0.5,
+    sellSize: 0.5,
+  }
+
+}
+
+const exchangeView = () => {
+  let boxStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '80%',
+    justifyContent: 'space-between',
+    backgroundColor: '#fef7ff',
+    margin: '10px',
+
+  }
+  let containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#fef1ff',
+    flexShrink: 3, /* default 1 */
+
+    //maxWidth: '75%',
+    //justifyContent: 'space-around'
+  }
+  
+  let exchanges = Object.keys(data);
+  let list = [];
+
+  for(let i = 0; i < exchanges.length; i++) {
+    let name = exchanges[i];
+    let { buyPrice, buySize } = data[name];
+    list.push(
+      <div key={i} style={boxStyle}>
+        <p>{name}</p>
+        <p>{buyPrice}</p>
+        <p>{buySize}</p>
+        <p>{buyPrice * buySize}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div style={containerStyle}>
+      {list}
+    </div>
+  );
+
+}
+
 const Offers = () => {
   return (
     <div style={offersContainer}>
@@ -29,6 +89,10 @@ const Offers = () => {
           <p>Price</p>
           <p>Amount</p>
           <p>Value</p>
+        </div>
+
+        <div>
+          {exchangeView()}
         </div>
 
       </div>
