c98b254623649536a9cdd1df085c1a9a5ea06b28
Melissa <lynch.melissat@gmail.com>
2021-12-03 11:06:29 -0500

    change app component styling from inline to stylesheet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
+import './App.css'
+
 import Offers from "./components/Offers";
 
-const container = {
-  display: 'flex',
-  flexDirection: 'row',
-  height: 500
-}
-
-const recs = {
-  height: '90%',
-  width: '40%',
-  backgroundColor: '#fef7ff',
-  borderRadius: '20px',
-  border: '3px solid pink',
-  margin: '10px',
-  padding: '10px'  
-}
 
 function App() {
   return (
     <div>
       <h1>Crypto Recs</h1>
-      <div style={container}>
+      <div className="container">
         <Offers /> 
-        <div style={recs}>Recommendations</div>
+        <div className="recs">Recommendations</div>
       </div>
     </div>
   );
